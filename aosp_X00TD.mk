9c348d0f2eb806617681dc228b0719a9af44461f
N4veenNK <naveen4264nk@gmail.com>
2022-03-06 13:44:16 +0000

    X00TD: Enable Quick Tap
    Signed-off-by: N4veenNK <naveen4264nk@gmail.com>

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -23,6 +23,7 @@ TARGET_BUILD_GRAPHENEOS_CAMERA := true
 TARGET_GAPPS_ARCH := arm64
 WITH_GAPPS := true
 COPY_APN_SYSTEM := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_X00TD
