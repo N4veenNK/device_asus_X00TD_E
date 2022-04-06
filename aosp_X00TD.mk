465a0145366559ee45995dee4e6af49fd355ecb4
N4veenNK <naveen4264nk@gmail.com>
2022-04-06 19:58:09 +0530

    X00TD: Inherit and Update Some Flags
    Signed-off-by: N4veenNK <naveen4264nk@gmail.com>

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -15,16 +15,6 @@ $(call inherit-product, vendor/aosp/common.mk)
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Aosp Stuff with GApps
-TARGET_BOOT_ANIMATION_RES := 1080
-ARCANA_DEVICE := X00TD
-TARGET_SUPPORTS_BLUR := false
-TARGET_BUILD_GRAPHENEOS_CAMERA := false
-TARGET_GAPPS_ARCH := arm64
-WITH_GAPPS := true
-COPY_APN_SYSTEM := true
-TARGET_SUPPORTS_QUICK_TAP := true
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_X00TD
 PRODUCT_DEVICE := X00TD
@@ -41,6 +31,20 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Build fingerprint
 BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys"
+
+# Arcana Stuff
+ARCANA_DEVICE := X00TD
+COPY_APN_SYSTEM := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_BLUR := false
+TARGET_SUPPORTS_NOW_PLAYING := true
+TARGET_SUPPORTS_QUICK_TAP := true
+USE_PIXEL_CHARGER_IMAGES := true
+                                                              # Gapps
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
 
 # MAINTAINER STUFF
 ARCANA_MAINTAINER := Naveen
